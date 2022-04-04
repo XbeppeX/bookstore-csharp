a818ccc418e73bcccef865ff598576ef66be68f3
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-04 14:27:43 +0200

    base+commenti

--- BookStoreCsharp/Program.cs
+++ BookStoreCsharp/Program.cs
@@ -10,7 +10,7 @@ namespace BookStoreCsharp
             string autore = "Robert C. “Uncle Bob” Martin";
             long isbn = 9780132350884;
             int numeroPagine = 431;
-            double pesoLibro = 660;
+            double pesoLibro = 0.660;
             double larghezzaLibro = 17.91;
             double altezzaLibro = 23.5;
             double profonditalibro = 2.54;
@@ -20,6 +20,16 @@ namespace BookStoreCsharp
             string copertinaFlessibile = "Si";
 
             Console.WriteLine("Il libro di oggi: " + titolo + " di " + autore);
+            Console.WriteLine("Informazioni generiche:");
+            Console.WriteLine("ISBN: " + isbn);
+            Console.WriteLine("Numero delle pagine: " + numeroPagine + " pagine");
+            Console.WriteLine("Peso del libro:: " + pesoLibro + "Kg");
+            Console.WriteLine("Dimensioni del libro: " + altezzaLibro + "cm x " + larghezzaLibro + "cm x " + profonditalibro + "cm");
+            Console.WriteLine("Informazioni Amazon:");
+            Console.WriteLine("Numero di recensioni: " + numeroRecensioni + " recensioni");
+            Console.WriteLine("Valutazione media: " + valutazioneMedia + "☆");
+            Console.WriteLine("Kindle disponibile: " + versioneKindle);
+            Console.WriteLine("Copertina flessibile disponibile: " + copertinaFlessibile);
 
         }
     }
